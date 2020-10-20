1029cd12b6e624aed0412a1829c959b274a30f4d
MatveyLh <liakh.matvei@mail.ru>
2020-10-20 21:24:56 +0300

    Updates

---
+++ precache-manifest.5e83cba2dd7a577baac5a7055bb54b16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6b8cd7a28fcb846a4dfe7add5c9d364",
+    "url": "/oziApp/index.html"
+  },
+  {
+    "revision": "7dd0a05272c06a66ea70",
+    "url": "/oziApp/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "5bb2012bca5a92ffc258",
+    "url": "/oziApp/static/css/main.3ba7431d.chunk.css"
+  },
+  {
+    "revision": "7dd0a05272c06a66ea70",
+    "url": "/oziApp/static/js/2.f872c35d.chunk.js"
+  },
+  {
+    "revision": "44d582a97d02337b347d751d7baba8fd",
+    "url": "/oziApp/static/js/2.f872c35d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bb2012bca5a92ffc258",
+    "url": "/oziApp/static/js/main.1d3204e5.chunk.js"
+  },
+  {
+    "revision": "580585accb606faf4ee0",
+    "url": "/oziApp/static/js/runtime-main.0e9fbfdf.js"
+  }
+]);
